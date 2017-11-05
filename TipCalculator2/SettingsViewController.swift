9fefb096776c4489a6afc190d0edaace5a0ee7ef
bigmak712 <bigmak712@gmail.com>
2017-11-05 11:42:35 -0800

    Finished settings functionality and saving info

--- TipCalculator2/SettingsViewController.swift
+++ TipCalculator2/SettingsViewController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var settingsTipControl: UISegmentedControl!
+    @IBOutlet weak var firstTipField: UITextField!
+    @IBOutlet weak var secondTipField: UITextField!
+    @IBOutlet weak var thirdTipField: UITextField!
+    
+    // Default tip percentages that are initially set
+    var tipPercentages = [0.15, 0.18, 0.2]
+    
+    // Default tip percentage that is currently being used
+    var tipPercent = 0.0
+    
+    // Default tip values
+    let defaultTip1 = 0.15
+    let defaultTip2 = 0.18
+    let defaultTip3 = 0.20
+    
+    // Used to save and load saved values
+    let defaults = UserDefaults.standard
+    
+    let tipKey = "defaultTip"
+    let customTipKey1 = "customTipKey1"
+    let customTipKey2 = "customTipKey2"
+    let customTipKey3 = "customTipKey3"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        firstTipField.darkUnderlineTextField()
+        secondTipField.darkUnderlineTextField()
+        thirdTipField.darkUnderlineTextField()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadTipPercentages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +54,137 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func setDefaultTip(_ sender: Any) {
+        // Set the default tip percentage and save it
+        defaults.set(tipPercentages[settingsTipControl.selectedSegmentIndex], forKey: tipKey)
+        defaults.synchronize()
     }
-    */
+    
+    @IBAction func changeCustomTip1(_ sender: Any) {
+        let tipValue = Double(firstTipField.text!) ?? 0
+        setCustomTip(tip: tipValue, index: 0)
+    }
+    @IBAction func changeCustomTip2(_ sender: Any) {
+        let tipValue = Double(secondTipField.text!) ?? 0
+        setCustomTip(tip: tipValue, index: 1)
+    }
+    @IBAction func changeCustomTip3(_ sender: Any) {
+        let tipValue = Double(thirdTipField.text!) ?? 0
+        setCustomTip(tip: tipValue, index: 2)
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    func setCustomTip(tip: Double, index: Int) {
+        
+        var customTipKey: String = ""
+        var defaultTip: Double
+        
+        if index == 0 {
+            customTipKey = customTipKey1
+            defaultTip = defaultTip1
+        }
+        else if index == 1 {
+            customTipKey = customTipKey2
+            defaultTip = defaultTip2
+        }
+        else {
+            customTipKey = customTipKey3
+            defaultTip = defaultTip3
+        }
+        
+        // tip1 value is between 0 and 100 exclusive
+        if(tip > 0 && tip < 100){
+            
+            // Set the first segment to the new tip value
+            settingsTipControl.setTitle(String(format: "%.0f", tip) + "%", forSegmentAt: index)
+            
+            // Divide the tip by 100 to get the percentage value
+            let tipPercentage = tip / 100
+            
+            // Update the tip percentage in the tipPercentages array
+            tipPercentages[index] = tipPercentage
+            
+            // Update the tip key if the selected tip was changed
+            if(settingsTipControl.selectedSegmentIndex == index){
+                defaults.set(tipPercentage, forKey: tipKey)
+            }
+            
+            // Save the new default tip percentage
+            defaults.set(tipPercentage, forKey: customTipKey)
+            defaults.synchronize()
+        }
+            
+        // tip is an invalid tip value
+        else{
+            
+            // Set the segment to the default tip value
+            settingsTipControl.setTitle(String(format: "%.0f", tipPercentages[index] * 100) + "%", forSegmentAt: index)
+            
+            // Update the tip percentage in the tipPercentages array to defaultTip
+            tipPercentages[index] = defaultTip
+            
+            // Update the tip key if the selected tip was changed
+            if(settingsTipControl.selectedSegmentIndex == index){
+                defaults.set(tipPercentages[index], forKey: tipKey)
+            }
+            
+            // Save the new default tip percentage
+            defaults.set(tipPercentages[index], forKey: customTipKey)
+            defaults.synchronize()
+            
+        }
+    }
+    
+    func loadTipPercentages() {
+        // Load the default tip percentages
+        var custom1 = defaults.double(forKey: customTipKey1)
+        var custom2 = defaults.double(forKey: customTipKey2)
+        var custom3 = defaults.double(forKey: customTipKey3)
+        
+        // If any tip percentages are 0%, set them to the default tip values
+        if(custom1 == 0){
+            custom1 = tipPercentages[0]
+        }
+        if(custom2 == 0){
+            custom2 = tipPercentages[1]
+        }
+        if(custom3 == 0){
+            custom3 = tipPercentages[2]
+        }
+        
+        // Update the tipPercentages array values
+        tipPercentages[0] = custom1
+        tipPercentages[1] = custom2
+        tipPercentages[2] = custom3
+        
+        // Multiply the default tip percentages by 100
+        custom1 = custom1 * 100
+        custom2 = custom2 * 100
+        custom3 = custom3 * 100
+        
+        // Set the segment control titles to the 3 default tip percentages
+        settingsTipControl.setTitle(String(format: "%.0f", custom1) + "%", forSegmentAt: 0)
+        settingsTipControl.setTitle(String(format: "%.0f", custom2) + "%", forSegmentAt: 1)
+        settingsTipControl.setTitle(String(format: "%.0f", custom3) + "%", forSegmentAt: 2)
+        
+        firstTipField.text = String(format: "%.0f", custom1)
+        secondTipField.text = String(format: "%.0f", custom2)
+        thirdTipField.text = String(format: "%.0f", custom3)
 
+        // Load the default tip key
+        tipPercent = defaults.double(forKey: tipKey)
+        
+        if(tipPercent == tipPercentages[0]){
+            settingsTipControl.selectedSegmentIndex = 0
+        }
+        else if(tipPercent == tipPercentages[1]){
+            settingsTipControl.selectedSegmentIndex = 1
+        }
+        else{
+            settingsTipControl.selectedSegmentIndex = 2
+        }
+    }
 }
--- TipCalculator2/TipViewController.swift
+++ TipCalculator2/TipViewController.swift
@@ -16,6 +16,20 @@ class TipViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var peopleField: UITextField!
     
+    // Default tip percentages that are initially set
+    var tipPercentages = [0.15, 0.18, 0.2]
+    
+    // Default tip percentage that is currently being used
+    var tipPercent = 0.0
+    
+    // Used to save and load saved values
+    let defaults = UserDefaults.standard
+    
+    let tipKey = "defaultTip"
+    let customTipKey1 = "customTipKey1"
+    let customTipKey2 = "customTipKey2"
+    let customTipKey3 = "customTipKey3"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +40,18 @@ class TipViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.endEditing(true)
+
+        billField.text = "$"
+        tipLabel.text = "$0.00"
+        peopleField.text = "1"
+        totalLabel.text = "$0.00 / $0.00"
+        
+        loadTipPercentages()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -45,9 +71,15 @@ class TipViewController: UIViewController {
         view.endEditing(true)
     }
     
-    @IBAction func calculateTip(_ sender: Any) {
+    @IBAction func tipPercentageChanged(_ sender: Any) {
+        // Set the default tip percentage and save it
+        defaults.set(tipPercentages[tipControl.selectedSegmentIndex], forKey: tipKey)
+        defaults.synchronize()
         
-        let tipPercentages = [0.18, 0.2, 0.15]
+        calculateTip(self)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
         
         let index = billField.text!.index(after: billField.text!.startIndex)
         let billString = billField.text![index...]
@@ -56,10 +88,65 @@ class TipViewController: UIViewController {
         let numPeople = Double(peopleField.text!) ?? 1
         
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = (bill + tip) / numPeople
+        let total = (bill + tip)
+        let splitTotal = total / numPeople
         
         tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        totalLabel.text = String(format: "$%.2f", total) + " / " + String(format: "$%.2f", splitTotal)
+    }
+    
+    func loadTipPercentages() {
+        // Get the custom tip percentages
+        var custom1 = defaults.double(forKey: customTipKey1)
+        var custom2 = defaults.double(forKey: customTipKey2)
+        var custom3 = defaults.double(forKey: customTipKey3)
+        
+        // If any tip percentages are 0%, set them to the default tip percentages
+        if(custom1 == 0){
+            custom1 = tipPercentages[0]
+        }
+        if(custom2 == 0){
+            custom2 = tipPercentages[1]
+        }
+        if(custom3 == 0){
+            custom3 = tipPercentages[2]
+        }
+        
+        // Set the values in the tipPercentages array
+        tipPercentages[0] = custom1
+        tipPercentages[1] = custom2
+        tipPercentages[2] = custom3
+        
+        // Load the default tip key
+        tipPercent = defaults.double(forKey: tipKey)
+        
+        // If tipPercent is 0, set it to default tip value 0.15 and save it
+        if(tipPercent == 0){
+            tipPercent = 0.15
+            defaults.set(tipPercent, forKey: tipKey)
+            defaults.synchronize()
+        }
+        
+        // Set the selected tipControl segment
+        if(tipPercent == tipPercentages[0]){
+            tipControl.selectedSegmentIndex = 0
+        }
+        else if(tipPercent == tipPercentages[1]){
+            tipControl.selectedSegmentIndex = 1
+        }
+        else{
+            tipControl.selectedSegmentIndex = 2
+        }
+        
+        // Multiply the custom tip percentages by 100
+        custom1 = custom1 * 100
+        custom2 = custom2 * 100
+        custom3 = custom3 * 100
+        
+        // Set the titles in the segment control to the custom tip percentages
+        tipControl.setTitle(String(format: "%.0f", custom1) + "%", forSegmentAt: 0)
+        tipControl.setTitle(String(format: "%.0f", custom2) + "%", forSegmentAt: 1)
+        tipControl.setTitle(String(format: "%.0f", custom3) + "%", forSegmentAt: 2)
     }
 }
 
@@ -68,6 +155,16 @@ extension UITextField{
         let border = CALayer()
         let width = CGFloat(0.75)
         border.borderColor = UIColor.white.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+    func darkUnderlineTextField(){
+        let border = CALayer()
+        let width = CGFloat(0.75)
+        border.borderColor = UIColor.darkGray.cgColor
         border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
         
         border.borderWidth = width
